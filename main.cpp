c6ae27c6ff2d9b41ecba45ce3043f483e061fbc1
RoselynHoffmann <rhoffman@LAPTOP-JCMC5UIH>
2023-05-17 02:17:20 +0930

    feat: added Pokemon class unfinished and untested

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "window.hpp"
+#include "game_container.hpp"
+
+int main()
+{
+	Game_container g1;
+
+	g1.show_game_menu();
+}
---
+++ pokemon.hpp
@@ -0,0 +1,63 @@
+/*****************************************************************************/
+/*                                                                           */
+/*         █████  ██████  ███████ ██       █████  ██ ██████  ███████         */
+/*        ██   ██ ██   ██ ██      ██      ██   ██ ██ ██   ██ ██              */
+/*        ███████ ██   ██ █████   ██      ███████ ██ ██   ██ █████           */
+/*        ██   ██ ██   ██ ██      ██      ██   ██ ██ ██   ██ ██              */
+/*        ██   ██ ██████  ███████ ███████ ██   ██ ██ ██████  ███████         */
+/*                                                                           */
+/*        ██    ██ ███    ██ ██    |                            |            */
+/*        ██    ██ ████   ██ ██    |   OOP Semester 1 2023      |            */
+/*        ██    ██ ██ ██  ██ ██    |   Camille, Rose, Alex      |            */
+/*        ██    ██ ██  ██ ██ ██    |   Pokemon Showdown remake  |            */
+/*         ██████  ██   ████ ██    |                            |            */
+/*                                                                           */
+/*        This is the Pokemon class responsible for the creation of          */
+/*      Pokemon, their respective stats, and stat changes in battle          */
+/*                                                                           */
+/*****************************************************************************/
+
+#ifndef POKEMON_H
+#define POKEMON_H
+
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <iostream>
+
+#include <string>
+
+#include "move.h"
+
+using std::string;
+using std::time;
+using std::rand;
+using std::cout;
+
+class Pokemon
+{
+	private: 
+		string name;
+		string type;
+		int max_hp;
+		int current_hp;
+		struct Stats
+    	{
+			int attack;
+			int defense;
+			int special_attack;
+			int special_defense;
+			int speed;
+    	} base_stats, mod_stats;
+	public:
+		virtual Pokemon();
+		void take_damage(int pwr);
+		void modify_stats(struct stats);
+		void receive_move(Move moves);
+		void set_name(string name);
+		void set_type(string type);
+		string get_name();
+		string get_type();
+};
+
+#endif
