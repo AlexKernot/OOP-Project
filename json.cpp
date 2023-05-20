d6b8f996a7e962336c2d0e71807fd0174618f98b
Alex Kernot <alex.kernot04@gmail.com>
2023-05-20 17:44:53 +0930

    Feat: Add JSON parsing

--- json.cpp
+++ json.cpp
@@ -1,36 +1,164 @@
 #include "json.hpp"
 
-bool JSON::LoadPokemonData(std::string name) {
-  std::ifstream file("resources/pokemonList.json");
-  json data;
-  try
-  {
-    data = json::parse(file);
+json JSON::ParseFile(std::string name) {
+  std::string fileName = "resources/" + name + ".json";
+  std::ifstream file(fileName);
+  if (file.fail()) {
+    std::cout << fileName << " could not be opened.\n";
+    parseSuccess = false;
+    return json(nullptr);
   }
-  catch(const json::parse_error e)
+  json tempData = json::parse(file, nullptr, false, true);
+  file.close();
+  if (tempData.is_discarded() == true)
   {
-    std::cerr << e.what() << '\n';
-    return false;
+    std::cout << "A syntax error was found in pokemonList.json";
+    parseSuccess = false;
+    return json(nullptr);
   }
-  std::vector<json> allPokemon = data.at("pokemon");
+  try {
+    tempData = tempData.at(name);
+  } catch (const json::out_of_range&) {
+    std::cout << "Missing " << name << " field in json.\n";
+    parseSuccess = false;
+    return json(nullptr);
+  }
+  parseSuccess = true;
+  return tempData;
+}
+
+JSON::JSON() {
+  json temp;
+  temp = ParseFile("pokemonList");
+  if (!temp.is_array())
+  {
+    parseSuccess = false;
+    return ;
+  }
+  pokemonData = temp;
+  temp = ParseFile("moveList");
+  if (!temp.is_array())
+  {
+    parseSuccess = false;
+    return ;
+  }
+  moveData = temp;
+}
+
+json JSON::LoadSinglePokemonData(std::string name) {
+  json tempPokemonName;
   json pokemon;
-  for (int i = 0; i < allPokemon.size(); ++i)
-  {
-    if (!allPokemon[i].at("name").is_string())
-    {
-      std::cout << "invalid sytax detected. expected: String, found: " 
-      << allPokemon[i].at("name").type_name() << " at index " << i << ": \n" 
-      << allPokemon[i].dump(2) << std::endl;
+  int size = static_cast<int>(pokemonData.size());
+  for (int i = 0; i < size; ++i) {
+    try {
+      tempPokemonName = pokemonData[i].at("name");
+    } catch (const json::out_of_range&) {
+      std::cout << "Missing \"name\" field at index: " << i;
+      return json(nullptr);
     }
-    if (allPokemon[i].at("name") == name)
-    {
-      pokemon = allPokemon[i];
+    if (!tempPokemonName.is_string()) {
+      GenerateTypeError(pokemonData[i], "String", "name");
+      return json(nullptr);
+    }
+    if (pokemonData[i].at("name") == name) {
+      pokemon = pokemonData[i];
       break;
     }
   }
   if (pokemon == nullptr)
     return false;
   std::string s_name = pokemon.at("name");
-  std::cout << s_name << std::endl;
   return true;
+}
+
+json JSON::GetField(json data, std::string name) {
+  json field;
+  try {
+    field = data.at(name);
+  } catch (const json::out_of_range&) {
+    std::cout << name << " field not found in " << data.at("name");
+    return json(nullptr);
+  }
+  return field;
+}
+
+Stats JSON::GetStatsJson(json statsJson) {
+  Stats pokemonStats = Stats(11, 4, 4, 4, 4, 4);
+  json tempStat;
+  tempStat = GetField(statsJson, "hp");
+  if (!tempStat.is_number_integer()) {
+    GenerateTypeError(statsJson, "number (integer)", "hp");
+    return pokemonStats;
+  }
+  pokemonStats.SetHP(tempStat);
+  tempStat = GetField(statsJson, "attack");
+  if (!tempStat.is_number_integer()) {
+    GenerateTypeError(statsJson, "number (integer)", "attack");
+    return pokemonStats;
+  }
+  pokemonStats.SetAttack(tempStat);
+  tempStat = GetField(statsJson, "defense");
+  if (!tempStat.is_number_integer()) {
+    GenerateTypeError(statsJson, "number (integer)", "defense");
+    return pokemonStats;
+  }
+  pokemonStats.SetDefense(tempStat);
+  tempStat = GetField(statsJson, "s_attack");
+  if (!tempStat.is_number_integer()) {
+    GenerateTypeError(statsJson, "number (integer)", "s_attack");
+    return pokemonStats;
+  }
+  pokemonStats.SetSpecialAttack(tempStat);
+  tempStat = GetField(statsJson, "s_defense");
+  if (!tempStat.is_number_integer()) {
+    GenerateTypeError(statsJson, "number (integer)", "s_defense");
+    return pokemonStats;
+  }
+  pokemonStats.SetDefense(tempStat);
+  tempStat = GetField(statsJson, "speed");
+  if (!tempStat.is_number_integer()) {
+    GenerateTypeError(statsJson, "number (integer)", "speed");
+    return pokemonStats;
+  }
+  pokemonStats.SetSpeed(tempStat);
+  return pokemonStats;
+}
+
+Pokemon *JSON::CreatePokemon(std::string name) {
+  json tempJson;
+  json pokemonJson = LoadSinglePokemonData(name);
+  if (pokemonJson.is_null() == true)
+    return nullptr;
+  tempJson = GetField(pokemonJson, "type_one");
+  if (!tempJson.is_string()) {
+    GenerateTypeError(pokemonJson, "string", "type_one");
+    return nullptr;
+  }
+  std::string type1 = tempJson;
+  tempJson = GetField(pokemonJson, "type_two");
+  if (!tempJson.is_string()) {
+    GenerateTypeError(pokemonJson, "string", "type_two");
+    return nullptr;
+  }
+  std::string type2 = tempJson;
+  json statsJson = GetField(pokemonJson, "stats");
+  if (!tempJson.is_array()) {
+    GenerateTypeError(pokemonJson, "array", "stats");
+    return nullptr;
+  }
+  Stats pokemonStats = GetStatsJson(statsJson);
+  std::vector<Move> moves = {
+    Move("Test1", 100, "None", "Normal", false),
+    Move("Test2", 100, "None", "Normal", false),
+    Move("Test3", 100, "None", "Normal", false),
+    Move("Test4", 100, "None", "Normal", false)};
+  Pokemon *pokemon = new Pokemon(name, Type(type1), Type(type2), pokemonStats, moves, 1);
+  return pokemon;
+}
+
+void JSON::GenerateTypeError(json data, std::string expectedType, std::string at)
+{
+  std::cout << "Invalid syntax detected. Expected: " << expectedType
+  << ", found: " << data.at(at).type_name()
+  << " at: \n" << data.dump(2) << std::endl;
 }
--- json.hpp
+++ json.hpp
@@ -5,13 +5,28 @@
 #include <nlohmann/json.hpp>
 #include <string>
 #include "pokemon.hpp"
+#include "type.hpp"
+#include "move.hpp"
 
 using json = nlohmann::json;
 
 class JSON {
+  vector<json> pokemonData;
+  vector<json> moveData;
+  bool parseSuccess = false;
 
 public:
-  bool LoadPokemonData(std::string name);
+  JSON();
+  Pokemon *CreatePokemon(std::string name);
+  bool GetParseResult() {return parseSuccess;}
+
+private:
+  void GenerateTypeError(json data, std::string expectedType, 
+                              std::string at);
+  json ParseFile(std::string fileName);
+  json LoadSinglePokemonData(std::string name);
+  Stats GetStatsJson(json statsJson);
+  json GetField(json data, std::string name);
 };
 
 #endif
