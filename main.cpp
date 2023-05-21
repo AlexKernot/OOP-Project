fd0b41c9e8c5d984bcdb7da78a3f4c052b2b5abf
Camille Nguyen <camillenguyen.nhbc@gmail.com>
2023-05-22 00:24:59 +0930

    docs: restore files

--- main.cpp
+++ main.cpp
@@ -1,32 +1,11 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <vector>
 #include "window.hpp"
-#include "player.hpp"
-#include "pokemon.hpp"
+#include "game_container.hpp"
+#include "menu.hpp"
+#include <iostream>
 
-
-int main() {
-    Window window;
-		vector<Pokemon*> pokemon;
-
-		Player p("Camille",pokemon);
-
-
-    while (window.sfWindow->isOpen()) {
-        sf::Event event;
-        while (window.PollEvent(&event)) {
-            if (event.type == sf::Event::Closed) {
-                window.~Window();
-            }
-            // Handle other events
-						p.make_choice(window.sfWindow);
-        }
-
-        window.RenderWindow();
-    }
-
-
-		
-    return 0;
+int main()
+{
+ 	Game_container game;
+ 	game.StartGame();
+ 	return 0;
 }
--- window.hpp
+++ window.hpp
@@ -20,29 +20,30 @@
 #ifndef POKEMON_WINDOW_H
 #define POKEMON_WINDOW_H
 
+#include "SFML/Window.hpp"
+
 #include <vector>
 
-#include "SFML/Window.hpp"
 #include "renderable.hpp"
 
 class Window {
- protected:
-  const int width = 800;
-  const int height = 600;
-  const int fpsLimit = 10;
-  std::vector<Renderable *> objects;
+private:
+	const int width = 800;
+	const int height = 600;
+	const int fpsLimit = 10;
+	std::vector<Renderable *> objects;
+	sf::RenderWindow *sfWindow;
 
- public:
-  sf::RenderWindow *sfWindow;
-  Window();
-  ~Window();
-  int GetWindowWidth() { return width; }
-  int GetWindowHeight() { return height; }
-  bool PollEvent(sf::Event *event);
-  void AddToWindow(Renderable *object);
-  void ClearFromWindow(Renderable *object);
-  void ClearEntireWindow();
-  void RenderWindow();
+public:
+	Window();
+	~Window();
+	int GetWindowWidth() {return width;}
+	int GetWindowHeight() {return height;}
+	bool PollEvent(sf::Event *event);
+	void AddToWindow(Renderable *object);
+	void ClearFromWindow(Renderable *object);
+	void ClearEntireWindow();
+	void RenderWindow();
 };
 
 #endif
