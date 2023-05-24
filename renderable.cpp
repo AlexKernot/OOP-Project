6a447e9a15cdfa066540c6d7ec8821895e3e4dd9
Camille Nguyen <camillenguyen.nhbc@gmail.com>
2023-05-24 22:10:09 +0930

    style: add comments

--- renderable.cpp
+++ renderable.cpp
@@ -1,7 +1,27 @@
+/*****************************************************************************/
+/*                                                                           */
+/*         █████  ██████  ███████ ██       █████  ██ ██████  ███████         */
+/*        ██   ██ ██   ██ ██      ██      ██   ██ ██ ██   ██ ██              */
+/*        ███████ ██   ██ █████   ██      ███████ ██ ██   ██ █████           */
+/*        ██   ██ ██   ██ ██      ██      ██   ██ ██ ██   ██ ██              */
+/*        ██   ██ ██████  ███████ ███████ ██   ██ ██ ██████  ███████         */
+/*                                                                           */
+/*        ██    ██ ███    ██ ██    |                            |            */
+/*        ██    ██ ████   ██ ██    |   OOP Semester 1 2023      |            */
+/*        ██    ██ ██ ██  ██ ██    |   Camille, Rose, Alex      |            */
+/*        ██    ██ ██  ██ ██ ██    |   Pokemon Showdown remake  |            */
+/*         ██████  ██   ████ ██    |                            |            */
+/*                                                                           */
+/*        This is the base class for all objects that need rendering         */
+/*      It is an abstract class that holds the sprites for each object       */
+/*                                                                           */
+/*****************************************************************************/
+
 #include "renderable.hpp"
 
-sf::Sprite *Renderable::add_sprite(std::string spriteName, 
-                                  std::string texturePath) {
+// Add a sprite to the sprite list and return a pointer to the added sprite
+sf::Sprite *Renderable::add_sprite(std::string spriteName,
+                                   std::string texturePath) {
   sprite_list.add_sprite(spriteName, texturePath);
   return sprite_list.get_sprite_name(spriteName);
 }
